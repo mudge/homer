ce8b11dca03d4aae0757dcc0c6bb73956d65ee23
Paul Mucur <mudge@mudge.name>
2018-12-31 15:14:33 +0000

    Add command line arguments

--- README.md
+++ README.md
@@ -2,6 +2,24 @@
 
 A minimal Cloudflare DNS-over-HTTPS ("DOH") proxy written in Rust.
 
+**Current version:** 0.1.0  
+**Supported Rust version:** 1.31
+
+## Usage
+
+```
+USAGE:
+    homer [OPTIONS]
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+
+OPTIONS:
+    -b, --bind <bind>            listen for DNS requests on this bind address and port [default: 127.0.0.1:53]
+    -u, --upstream <upstream>    URL of upstream DNS-over-HTTPS service [default: https://1.1.1.1/dns-query]
+```
+
 * https://developers.cloudflare.com/1.1.1.1/dns-over-https/
 
 ## License
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,33 @@
 use std::net::UdpSocket;
 
+use clap::{App, Arg};
+
 fn main() {
     {
-        let socket = UdpSocket::bind("127.0.0.1:5053").expect("Could not bind to 5053, is something else listening?");
+        let matches = App::new("Homer")
+            .version("0.1")
+            .about("DNS-over-HTTPS proxy")
+            .arg(Arg::with_name("bind")
+                 .help("listen for DNS requests on this bind address and port")
+                 .takes_value(true)
+                 .short("b")
+                 .long("bind")
+                 .default_value("127.0.0.1:53"))
+            .arg(Arg::with_name("upstream")
+                 .help("URL of upstream DNS-over-HTTPS service")
+                 .takes_value(true)
+                 .short("u")
+                 .long("upstream")
+                 .default_value("https://1.1.1.1/dns-query"))
+            .get_matches();
+
+        let bind_address = matches.value_of("bind").expect("No bind address found");
+        let upstream_url = matches.value_of("upstream").expect("No upstream DOH service URL found");
+
+        let socket = UdpSocket::bind(bind_address).expect(&format!("Could not bind to {}, is something already listening on that address?", bind_address));
         let client = reqwest::Client::new();
 
-        println!("Listening for DNS requests on 127.0.0.1:5053...");
+        println!("Listening for DNS requests on {}...", bind_address);
 
         loop {
             // DNS messages shouldn't be more than 512 bytes in length.
@@ -14,7 +36,7 @@ fn main() {
             if let Ok((number_of_bytes, src_addr)) = socket.recv_from(&mut buf) {
                 let filled_buf = &mut buf[..number_of_bytes];
 
-                let result = client.post("https://1.1.1.1/dns-query")
+                let result = client.post(upstream_url)
                     .header(reqwest::header::CONTENT_TYPE, "application/dns-message")
                     .body(filled_buf.to_vec())
                     .send();
@@ -29,10 +51,10 @@ fn main() {
                                     println!("error sending response to client: {:?}", e);
                                 }
                             }
-                            Err(e) => println!("error reading response from Cloudflare: {:?}", e)
+                            Err(e) => println!("error reading response from {}: {:?}", upstream_url, e)
                         }
                     }
-                    Err(e) => println!("error sending DNS request to Cloudflare: {:?}", e)
+                    Err(e) => println!("error sending DNS request to {}: {:?}", upstream_url, e)
                 }
             }
         }
