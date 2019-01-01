051ace233fec788541d85c65b5a876295ed0e70a
Paul Mucur <mudge@mudge.name>
2019-01-01 11:55:55 +0000

    Simplify before deploy script further

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ A minimal Cloudflare DNS-over-HTTPS ("DOH") proxy written in Rust.
 **Current version:** 0.1.0  
 **Supported Rust version:** 1.31
 
+## Install
+
+Download the latest binary from the [releases page](https://github.com/mudge/homer/releases).
+
 ## Usage
 
 ```
@@ -19,6 +23,8 @@ OPTIONS:
     -b, --bind <bind>            listen for DNS requests on this bind address and port [default: 127.0.0.1:53]
     -u, --upstream <upstream>    URL of upstream DNS-over-HTTPS service [default: https://1.1.1.1/dns-query]
 ```
+
+## References
 
 * https://developers.cloudflare.com/1.1.1.1/dns-over-https/
 
--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -1,24 +1,16 @@
-set -ex
+#!/bin/bash
+set -euo pipefail
 
-main() {
-    local src=$(pwd) \
-          stage=$(mktemp -d)
+# Install the Rust stdlib for ARMv6
+rustup target install arm-unknown-linux-gnueabihf
 
-    rustup target install arm-unknown-linux-gnueabihf
+# Download the Raspberry Pi cross-compilation toolchain
+git clone --depth=1 https://github.com/raspberrypi/tools.git /tmp/tools
 
-    git clone --depth=1 https://github.com/raspberrypi/tools.git
-    export PATH=$src/tools/arm-bcm2708/arm-linux-gnueabihf/bin:$PATH
-    export CARGO_TARGET_ARM_UNKNOWN_LINUX_GNUEABIHF_LINKER=arm-linux-gnueabihf-gcc
+# Compile the binary for the Raspberry Pi
+env PATH=/tmp/tools/arm-bcm2708/arm-linux-gnueabihf/bin:$PATH \
+  CARGO_TARGET_ARM_UNKNOWN_LINUX_GNUEABIHF_LINKER=arm-linux-gnueabihf-gcc \
+  cargo build --target=arm-unknown-linux-gnueabihf --release
 
-    cargo build --target=arm-unknown-linux-gnueabihf --release
-
-    cp target/arm-unknown-linux-gnueabihf/release/homer $stage/
-
-    cd $stage
-    tar czf $src/homer-$TRAVIS_TAG-arm-unknown-linux-gnueabihf.tar.gz *
-    cd $src
-
-    rm -rf $stage
-}
-
-main
+# Package up the release binary
+tar -C target/arm-unknown-linux-gnueabihf/release -czf homer-$TRAVIS_TAG-arm-unknown-linux-gnueabihf.tar.gz homer
