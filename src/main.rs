57e9478bc3c4510192b2d8b529fd3318d0e4e3a6
Paul Mucur <mudge@mudge.name>
2019-01-01 12:22:36 +0000

    Remove redundant block from main function

--- src/main.rs
+++ src/main.rs
@@ -10,26 +10,24 @@ mod udp_server;
 mod upstream;
 
 fn main() {
-    {
-        let args = Args::parse();
+    let args = Args::parse();
 
-        let socket = UdpSocket::bind(&args.bind)
-            .expect("could not bind to bind address, is something already listening?");
+    let socket = UdpSocket::bind(&args.bind)
+        .expect("could not bind to bind address, is something already listening?");
 
-        println!("Listening for DNS requests on {}...", &args.bind);
+    println!("Listening for DNS requests on {}...", &args.bind);
 
-        let server = UdpServer::new(&socket);
+    let server = UdpServer::new(&socket);
 
-        let client = reqwest::Client::new();
-        let upstream = Upstream::new(&client, &args.upstream);
+    let client = reqwest::Client::new();
+    let upstream = Upstream::new(&client, &args.upstream);
 
-        for request in server {
-            if let Err(e) = upstream
-                .send(&request)
-                .map(|response| server.reply(&request, response.as_slice()))
-            {
-                eprintln!("error during DNS request: {:?}", e);
-            }
+    for request in server {
+        if let Err(e) = upstream
+            .send(&request)
+            .map(|response| server.reply(&request, response.as_slice()))
+        {
+            eprintln!("error during DNS request: {:?}", e);
         }
     }
 }
